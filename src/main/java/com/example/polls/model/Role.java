d377f10fc50ee890500d3a3aa82edb724cdcad41
Ahmad Habibzad <masoud.habibzad@gmail.com>
2021-05-20 09:15:54 -0400

    Add security config

--- src/main/java/com/example/polls/model/Role.java
+++ src/main/java/com/example/polls/model/Role.java
@@ -41,4 +41,30 @@ public class Role {
     public Role(RoleName name) {
         this.name = name;
     }
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public RoleName getName() {
+		return name;
+	}
+
+	public void setName(RoleName name) {
+		this.name = name;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+    
+    
 }
